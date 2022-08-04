d29efee680709ba57f0a056ee5aeb26575fa7b13
Algasami <105358814+algasami@users.noreply.github.com>
2022-08-04 21:08:14 +0800

    Puttin' it in the work 🧪

--- sim_1/new/simu.v
+++ sim_1/new/simu.v
@@ -5,24 +5,24 @@ module alu_tb;
     reg [31:0] b;
     reg [2:0] op;
     wire [31:0] out;
-    
+
     alu _alu(a, b, op, out);
-    
+
     initial begin
         a = 32'h0; b = 32'h0; op = 3'h0;        // add
     end
-    
+
     initial begin
         #1000 a = 32'h0; b = 32'h0; op = 3'h1;  // sub
         #1000 a = 32'h0; b = 32'h0; op = 3'h2;  // and
         #1000 a = 32'h0; b = 32'h0; op = 3'h3;  // or
         #1000 a = 32'h0; b = 32'h0; op = 3'h4;  // xor
     end
-    
+
     always begin
         #1 a = a + 32'h1; b = b + 32'h1;
     end
-    
+
     initial #5000 $stop;
-    
+
 endmodule
--- sources_1/new/alu.v
+++ sources_1/new/alu.v
@@ -1,55 +1,53 @@
 `include "define.v"
 
+`define sizebus 31:0
 `define opbus 6:0
 `define f7bus 16:10
 `define f3bus 9:7
-module alu(input [31:0] a, input [31:0] b, input [16:0] op,output reg[31:0] y);
+module alu(input [`sizebus] a, input [`sizebus] b, input [16:0] op,output reg[`sizebus] y);
+
+    wire [`sizebus] addResult;  assign addResult = a + b;
+    wire [`sizebus] subResult;  assign subResult = a - b;
+    wire [`sizebus] ultResult;  assign ultResult = (a < b) ? 32'd1 : 0;
+    wire [`sizebus] sltResult;  assign sltResult = ($signed(a) < $signed(b)) ? 32'd1 : 0;
+    wire [`sizebus] xorResult;  assign xorResult = a ^ b;
+    wire [`sizebus] orResult;   assign orResult = a | b;
+    wire [`sizebus] andResult;  assign andResult = a & b;
     always@(*)begin
-        case(op[`opbus]) // opcode
+        case(op[`opbus])
             `RTYPE: begin
                 case(op[`f3bus])
                     3'b000: begin
                         case(op[`f7bus])
-                            7'b0000000: y = a + b;
-                            7'b0100000: y = a - b;
+                            7'b0000000: y = addResult;
+                            7'b0100000: y = subResult;
                         endcase
                     end
-                    3'b001: y = a << (b[4:0]);
-                    3'b010: y = $signed(a) < $signed(b);
-                    3'b011: y = a < b;
-                    3'b100: y = a ^ b;
+                    3'b001: y = a << b[4:0];
+                    3'b010: y = sltResult;
+                    3'b011: y = ultResult;
+                    3'b100: y = xorResult;
                     3'b101: begin
                         case(op[`f7bus])
                             7'b0000000: y = a >> b;
                             7'b0100000: y = $signed(a) >>> b;
                         endcase
                     end
-                    3'b110: y = a | b;
-                    3'b111: y = a & b;
+                    3'b110: y = orResult;
+                    3'b111: y = andResult;
                 endcase
             end
             `ITYPE: begin
                 case(op[`f3bus])
-                    3'b000: y = a + b;
-                    3'b010: y = $signed(a) < $signed(b);
-                    3'b011: y = a < b;
-                    3'b100: y = a ^ b;
-                    3'b110: y = a | b;
-                    3'b111: y = a & b;
+                    3'b000: y = addResult;
+                    3'b010: y = sltResult;
+                    3'b011: y = ultResult;
+                    3'b100: y = xorResult;
+                    3'b110: y = orResult;
+                    3'b111: y = andResult;
                 endcase
             end
             default: y = 0;
         endcase
     end
 endmodule
-
-// `ADD_OP: y = a + b;
-// `SUB_OP: y = a - b;
-// `AND_OP: y = a & b;
-// `OR_OP : y = a | b;
-// `XOR_OP: y = a ^ b;
-// `SHL_OP: y = a << b;
-// `SHR_OP: y = a >> b;
-// `LT_OP:  y = (a < b) ? 32'd1 : 32'd0;
-// `SLT_OP:  y = ($signed(a) < $signed(b)) ? 32'd1 : 32'd0;
-// default: y = 0;
